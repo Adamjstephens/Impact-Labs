c4a6a0309166de1a7c6d8c7ebc0584701e1484f8
adamcreativ11 <sa97141@mail.harpercollege.edu>
2023-11-17 12:29:22 -0600

    second commit

--- app.js
+++ app.js
@@ -5,13 +5,17 @@ document.addEventListener("DOMContentLoaded", () => {
 function generateList() {
     const gameName = document.getElementById("gameName").value;
     if (gameName) {
-        const listContainer = document.getElementById("lists");
+        const listsContainer = document.getElementById("lists");
+        const listId = gameName.toLowerCase().replace(/\s/g, '-');
         const list = document.createElement("div");
-        list.innerHTML = `<h2>${gameName}</h2>
-                          <ol id="${gameName.toLowerCase().replace(/\s/g, '-')}-list"></ol>`;
-        listContainer.appendChild(list);
+        const timestamp = new Date().toLocaleString(); // Get the current timestamp
+
+        list.innerHTML = `<h2>${gameName} - ${timestamp}</h2>
+                          <ol id="${listId}-list"></ol>
+                          <button onclick="removeList('${listId}')">Remove List</button>`;
+        listsContainer.appendChild(list);
         createTasksList(gameName);
-        saveList(gameName);
+        saveList(gameName, timestamp);
     } else {
         alert("Please enter a game name.");
     }
@@ -26,7 +30,7 @@ function createTasksList(gameName) {
         const checkbox = document.createElement("input");
         checkbox.type = "checkbox";
         checkbox.id = `${gameName.replace(/\s/g, '-')}-task-${index}`;
-        checkbox.addEventListener("change", () => updateTaskStatus(gameName, index, checkbox.checked));
+        checkbox.addEventListener("change", () => updateTaskStatus(gameName, index, checkbox));
 
         const label = document.createElement("label");
         label.htmlFor = `${gameName.replace(/\s/g, '-')}-task-${index}`;
@@ -38,18 +42,27 @@ function createTasksList(gameName) {
     });
 }
 
-function updateTaskStatus(gameName, index, isChecked) {
+function updateTaskStatus(gameName, index, checkbox) {
     const lists = JSON.parse(localStorage.getItem("lists")) || [];
     const tasks = lists.find(item => item.gameName === gameName).tasks;
-    tasks[index].completed = isChecked;
+    tasks[index].completed = checkbox.checked;
+
+    // Add timestamp logic
+    if (checkbox.checked) {
+        tasks[index].timestamp = new Date().toLocaleString();
+    } else {
+        tasks[index].timestamp = null; // Reset timestamp if unchecked
+    }
+
     localStorage.setItem("lists", JSON.stringify(lists));
 }
 
-function saveList(gameName) {
+function saveList(gameName, timestamp) {
     const lists = JSON.parse(localStorage.getItem("lists")) || [];
     const newGame = {
         gameName: gameName,
-        tasks: Array(4).fill({ completed: false })
+        timestamp: timestamp,
+        tasks: Array(4).fill({ completed: false, timestamp: null })
     };
     lists.push(newGame);
     localStorage.setItem("lists", JSON.stringify(lists));
@@ -57,12 +70,14 @@ function saveList(gameName) {
 
 function loadLists() {
     const lists = JSON.parse(localStorage.getItem("lists")) || [];
-    const listContainer = document.getElementById("lists");
+    const listsContainer = document.getElementById("lists");
     lists.forEach(game => {
+        const listId = game.gameName.toLowerCase().replace(/\s/g, '-');
         const list = document.createElement("div");
-        list.innerHTML = `<h2>${game.gameName}</h2>
-                          <ol id="${game.gameName.toLowerCase().replace(/\s/g, '-')}-list"></ol>`;
-        listContainer.appendChild(list);
+        list.innerHTML = `<h2>${game.gameName} - ${game.timestamp}</h2>
+                          <ol id="${listId}-list"></ol>
+                          <button onclick="removeList('${listId}')">Remove List</button>`;
+        listsContainer.appendChild(list);
         createTasksList(game.gameName);
         game.tasks.forEach((task, index) => {
             const checkbox = document.getElementById(`${game.gameName.replace(/\s/g, '-')}-task-${index}`);
@@ -73,10 +88,10 @@ function loadLists() {
     });
 }
 
-function removeAllLists() {
-    const confirmRemove = window.confirm("Are you sure you want to remove all lists?");
-    if (confirmRemove) {
-        localStorage.removeItem("lists");
-        document.getElementById("lists").innerHTML = "";
-    }
+function removeList(listId) {
+    const lists = JSON.parse(localStorage.getItem("lists")) || [];
+    const updatedLists = lists.filter(item => item.gameName.toLowerCase().replace(/\s/g, '-') !== listId);
+    localStorage.setItem("lists", JSON.stringify(updatedLists));
+    document.getElementById("lists").innerHTML = ""; // Clear existing lists
+    loadLists(); // Reload the lists after removal
 }
